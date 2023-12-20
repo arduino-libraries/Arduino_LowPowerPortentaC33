63d4c47ec83f934e15adf2a0f04959a2068dd636
AliceCrisu <cristidragomir97@gmail.com>
2023-12-20 11:57:22 +0200

    added setWakeupRTC() method

--- LowPower.cpp
+++ LowPower.cpp
@@ -1,10 +1,14 @@
 #include "LowPower.h"
 
+void LowPower::delay(long ms){
+
+}
+
 LowPower::LowPower(){
     lpm_voluntary_sleep_cfg.dtc_state_in_snooze = LPM_SNOOZE_DTC_DISABLE; // LPM_SNOOZE_DTC_ENABLE LPM_SNOOZE_DTC_DISABLE
-    lpm_voluntary_sleep_cfg.power_supply_state = LPM_POWER_SUPPLY_DEEPCUT0; // LPM_POWER_SUPPLY_DEEPCUT0, LPM_POWER_SUPPLY_DEEPCUT1, LPM_POWER_SUPPLY_DEEPCUT3
-    lpm_voluntary_sleep_cfg.output_port_enable = LPM_OUTPUT_PORT_ENABLE_RETAIN; // LPM_OUTPUT_PORT_ENABLE_RETAIN LPM_OUTPUT_PORT_ENABLE_HIGH_IMPEDANCE
-    lpm_voluntary_sleep_cfg.io_port_state = LPM_IO_PORT_NO_CHANGE;  // LPM_IO_PORT_NO_CHANGE LPM_IO_PORT_RESET
+    lpm_voluntary_sleep_cfg.power_supply_state = LPM_POWER_SUPPLY_DEEPCUT3; // LPM_POWER_SUPPLY_DEEPCUT0, LPM_POWER_SUPPLY_DEEPCUT1, LPM_POWER_SUPPLY_DEEPCUT3
+    lpm_voluntary_sleep_cfg.output_port_enable = LPM_OUTPUT_PORT_ENABLE_HIGH_IMPEDANCE; // LPM_OUTPUT_PORT_ENABLE_RETAIN LPM_OUTPUT_PORT_ENABLE_HIGH_IMPEDANCE
+    lpm_voluntary_sleep_cfg.io_port_state = LPM_IO_PORT_RESET;  // LPM_IO_PORT_NO_CHANGE LPM_IO_PORT_RESET
 
 }
 
@@ -20,12 +24,17 @@ void LowPower::deepSleep(){
     R_LPM_LowPowerModeEnter(&lpm_voluntary_sleep_ctrl);
 }
 
+void LowPower::setWakeupRTC(){
+    lpm_voluntary_sleep_cfg.deep_standby_cancel_source = LPM_DEEP_STANDBY_CANCEL_SOURCE_RTC_ALARM;
+    lpm_voluntary_sleep_cfg.standby_wake_sources = LPM_STANDBY_WAKE_SOURCE_RTCALM ;
+}
+
 void LowPower::setWakeupPin(uint8_t pin){
     attachInterrupt(digitalPinToInterrupt(pin), nullptr, RISING);
     deepSleepWakeupSource = deepSleepWakeupSource | getDeepSleepWakeIRQChannelForPin(pin);
     standbyWakeupSource = standbyWakeupSource | getStandbyWakeIRQChannelForPin(pin);
     lpm_voluntary_sleep_cfg.deep_standby_cancel_source = deepSleepWakeupSource;
-    lpm_voluntary_sleep_cfg.standby_wake_sources = standby
+    lpm_voluntary_sleep_cfg.standby_wake_sources = standbyWakeupSource;
 }
 
 int LowPower::getStandbyWakeIRQChannelForPin(uint8_t pin){
